1a276dd70c759946825c565a809020397c49e406
gogowonji <65698313+gogowonji@users.noreply.github.com>
2021-08-18 21:01:44 +0900

    API key 삭제

--- youtube/Constants.swift
+++ youtube/Constants.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct Constants{
-    static var API_KEY = "AIzaSyBglBYJKlIbuWCU-F0EJmqkIJZ8TDlwbTc"
+    static var API_KEY = ""
     static var PLAYLIST_ID = "UU2D6eRvCeMtcF5OGHf1-trw"
     static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
     
